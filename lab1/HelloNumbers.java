bb2524bbd8bbab4eef5495d97bcdf12d486f7205
Xueqian Hu <731954402@qq.com>
2021-04-15 17:02:20 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,9 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        for (int i = 0; x < 45; i += 1) {
+        	x += i;
+        	System.out.print(x + " ");
+        }
+    }
+}
