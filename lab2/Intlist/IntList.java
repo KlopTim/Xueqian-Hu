5f385658d2d67cfc2885cf2144f6803287185118
Xueqian Hu <731954402@qq.com>
2021-04-24 00:48:28 +0800

    completed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,18 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+        if (B == null) {
+            return A;
+        }
+        IntList p = A;
+        while (p.rest != null) {
+            p = p.rest;
+        }
+        p.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +102,36 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        // copy A
+        IntList A_copy;
+        if (A == null) {
+            A_copy = null;
+        } else {
+            A_copy = new IntList(A.first, null);
+            IntList p = A_copy;
+            A = A.rest;
+            while (A != null) {
+                p.rest = new IntList(A.first, null);
+                p = p.rest;
+                A = A.rest;
+            }
+        }
+        // copy B
+        IntList B_copy;
+        if (B == null) {
+            B_copy = null;
+        } else {
+            B_copy = new IntList(B.first, null);
+            IntList p = B_copy;
+            B = B.rest;
+            while (B != null) {
+                p.rest = new IntList(B.first, null);
+                p = p.rest;
+                B = B.rest;
+            }
+        }
+        // catenate A and B
+        return IntList.dcatenate(A_copy, B_copy);
     }
 
 
